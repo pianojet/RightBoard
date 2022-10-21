2a3a8198bfc409e6fe4c98f1d2e0e08a06577ada
Justin Taylor <pianojet@gmail.com>
2022-10-21 02:37:51 +0000

    initial commit

---
+++ src/components/__tests__/Board.spec.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from "vitest";
+
+import { mount } from "@vue/test-utils";
+import Board from "../Board.vue";
+
+describe("Board", () => {
+  it("renders properly", () => {
+    // const wrapper = mount(Board, { props: { msg: "Hello Vitest" } });
+    // expect(wrapper.text()).toContain("Hello Vitest");
+  });
+});
---
+++ src/views/Display.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Board from "../components/Board.vue";
+</script>
+
+<template>
+  <main>
+    <Board />
+  </main>
+</template>
